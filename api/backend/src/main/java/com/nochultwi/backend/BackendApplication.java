80993261720e516299539d04ffb7c3191e0c40bc
kid03181 <109463455+kid03181@users.noreply.github.com>
2026-09-13 04:03:16 +0900

    chore(structure): 백엔드 프로젝트 초기화

---
+++ api/backend/src/main/java/com/nochultwi/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.nochultwi.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
